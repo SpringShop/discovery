11af129c1c6e2ba1053d43673a7674f5322afc72
Taher Uddin <ktaheruddin@gmail.com>
2020-08-05 19:31:22 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Configure eureka client as below-
+
+`eureka.client.service-url:defaultZone: http://admin:admin@localhost:8761/discovery/eureka/`  
---
+++ src/main/java/com/taheruddin/discovery/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.taheruddin.discovery;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        // http.csrf().disable(); // Not good for production
+        super.configure(http);
+    }
+}
